d3ff9219fc3c9eb09bc2a7017f0931ded7ee492b
ShockByte <darknetsoft@yandex.ru>
2019-05-12 23:38:52 +0300

    Init data

--- Triangulation/Form1.Designer.cs
+++ Triangulation/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MainImage = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.triangulateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenImg = new System.Windows.Forms.OpenFileDialog();
+            this.SaveImg = new System.Windows.Forms.SaveFileDialog();
+            this.drawPolygonsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.MainImage)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainImage
+            // 
+            this.MainImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainImage.Location = new System.Drawing.Point(0, 24);
+            this.MainImage.Name = "MainImage";
+            this.MainImage.Size = new System.Drawing.Size(800, 464);
+            this.MainImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MainImage.TabIndex = 0;
+            this.MainImage.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.triangulateToolStripMenuItem,
+            this.drawPolygonsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // triangulateToolStripMenuItem
+            // 
+            this.triangulateToolStripMenuItem.Name = "triangulateToolStripMenuItem";
+            this.triangulateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.triangulateToolStripMenuItem.Text = "Triangulate";
+            this.triangulateToolStripMenuItem.Click += new System.EventHandler(this.triangulateToolStripMenuItem_Click);
+            // 
+            // drawPolygonsToolStripMenuItem
+            // 
+            this.drawPolygonsToolStripMenuItem.Name = "drawPolygonsToolStripMenuItem";
+            this.drawPolygonsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.drawPolygonsToolStripMenuItem.Text = "Draw Polygons";
+            this.drawPolygonsToolStripMenuItem.Click += new System.EventHandler(this.drawPolygonsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 488);
+            this.Controls.Add(this.MainImage);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Image Triangulation";
+            ((System.ComponentModel.ISupportInitialize)(this.MainImage)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox MainImage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem triangulateToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog OpenImg;
+        private System.Windows.Forms.SaveFileDialog SaveImg;
+        private System.Windows.Forms.ToolStripMenuItem drawPolygonsToolStripMenuItem;
     }
 }
 
--- Triangulation/Form1.cs
+++ Triangulation/Form1.cs
@@ -1,20 +1,234 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Imaging;
+using TriangleNet;
+using TriangleNet.Geometry;
+using TriangleNet.Topology;
+using Image = System.Drawing.Image;
 
 namespace Triangulation
 {
     public partial class Form1 : Form
     {
+        private Mesh triangulated = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenImg.FileName = "";
+            if (OpenImg.ShowDialog() != DialogResult.OK)
+                return;
+
+            try
+            {
+                MainImage.Image = Image.FromFile(OpenImg.FileName);
+            }
+            catch
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveImg.FileName = "";
+            if (SaveImg.ShowDialog() != DialogResult.OK)
+                return;
+
+            try
+            {
+                MainImage.Image.Save(SaveImg.FileName, ImageFormat.Png);
+
+            }
+            catch
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void triangulateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if(MainImage.Image == null)
+                return;
+
+            byte[,,] imageBits = BitmapToByteRgbQ((Bitmap)MainImage.Image);
+
+            Polygon p = new Polygon();
+
+            List<Vertex> corners;
+            if (MessageBox.Show("Использовать глубокий поиск границ?", "Хмъ", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Asterisk) == DialogResult.Yes)
+            {
+                MoravecCornersDetector scd = new MoravecCornersDetector();
+                corners = scd.ProcessImage((Bitmap)MainImage.Image).Select(x => new Vertex(x.X, x.Y)).ToList();
+            }
+            else
+            {
+                SusanCornersDetector scd = new SusanCornersDetector();
+                corners = scd.ProcessImage((Bitmap)MainImage.Image).Select(x => new Vertex(x.X, x.Y)).ToList();
+            }
+
+            corners.ForEach(delegate (Vertex v)
+            {
+                p.Add(v);
+            });
+
+            triangulated = (Mesh)p.Triangulate();
+
+
+            using (Graphics graphics = Graphics.FromImage(MainImage.Image))
+            {
+                foreach (Triangle t in triangulated.Triangles)
+                {
+                    PointF[] points =
+                    {
+                        new PointF((float) t.GetVertex(0).X, (float) t.GetVertex(0).Y),
+                        new PointF((float) t.GetVertex(1).X, (float) t.GetVertex(1).Y),
+                        new PointF((float) t.GetVertex(2).X, (float) t.GetVertex(2).Y),
+                    };
+
+                    int ColR = imageBits[0, (int)points[0].Y, (int)points[0].X] +
+                               imageBits[0, (int)points[1].Y, (int)points[1].X] +
+                               imageBits[0, (int)points[2].Y, (int)points[2].X];
+
+                    int ColG = imageBits[1, (int)points[0].Y, (int)points[0].X] +
+                               imageBits[1, (int)points[1].Y, (int)points[1].X] +
+                               imageBits[1, (int)points[2].Y, (int)points[2].X];
+
+                    int ColB = imageBits[2, (int)points[0].Y, (int)points[0].X] +
+                               imageBits[2, (int)points[1].Y, (int)points[1].X] +
+                               imageBits[2, (int)points[2].Y, (int)points[2].X];
+
+
+                    Brush brush = new SolidBrush(Color.FromArgb(ColR / 3, ColG / 3, ColB / 3));
+                    graphics.FillPolygon(brush, points);
+                }
+            }
+            MainImage.Invalidate();
+        }
+
+        public unsafe byte[,,] BitmapToByteRgbQ(Bitmap bmp)
+        {
+            int width = bmp.Width,
+                height = bmp.Height;
+            byte[,,] res = new byte[3, height, width];
+            BitmapData bd = bmp.LockBits(new System.Drawing.Rectangle(0, 0, width, height), ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+            try
+            {
+                byte* curpos;
+                fixed (byte* _res = res)
+                {
+                    byte* _r = _res, _g = _res + width * height, _b = _res + 2 * width * height;
+                    for (int h = 0; h < height; h++)
+                    {
+                        curpos = ((byte*)bd.Scan0) + h * bd.Stride;
+                        for (int w = 0; w < width; w++)
+                        {
+                            *_b = *(curpos++);
+                            ++_b;
+                            *_g = *(curpos++);
+                            ++_g;
+                            *_r = *(curpos++);
+                            ++_r;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                bmp.UnlockBits(bd);
+            }
+            return res;
+        }
+
+
+        public void Bresenham4Line(Graphics g, Color clr, int x0, int y0, int x1, int y1)
+        {
+            int dx = (x1 > x0) ? (x1 - x0) : (x0 - x1);
+            int dy = (y1 > y0) ? (y1 - y0) : (y0 - y1);
+            int sx = (x1 >= x0) ? (1) : (-1);
+            int sy = (y1 >= y0) ? (1) : (-1);
+
+            if (dy < dx)
+            {
+                int d = (dy << 1) - dx;
+                int d1 = dy << 1;
+                int d2 = (dy - dx) << 1;
+                PutPixel(g, clr, x0, y0, 255);
+                int x = x0 + sx;
+                int y = y0;
+                for (int i = 1; i <= dx; i++)
+                {
+                    if (d > 0)
+                    {
+                        d += d2;
+                        y += sy;
+                    }
+                    else
+                        d += d1;
+                    PutPixel(g, clr, x, y, 255);
+                    x += sx;
+                }
+            }
+            else
+            {
+                int d = (dx << 1) - dy;
+                int d1 = dx << 1;
+                int d2 = (dx - dy) << 1;
+                PutPixel(g, clr, x0, y0, 255);
+                int x = x0;
+                int y = y0 + sy;
+                for (int i = 1; i <= dy; i++)
+                {
+                    if (d > 0)
+                    {
+                        d += d2;
+                        x += sx;
+                    }
+                    else
+                        d += d1;
+                    PutPixel(g, clr, x, y, 255);
+                    y += sy;
+                }
+            }
+        }
+
+        private static void PutPixel(Graphics g, Color col, int x, int y, int alpha)
+        {
+            g.FillRectangle(new SolidBrush(Color.FromArgb(alpha, col)), x, y, 1, 1);
+        }
+
+        private void drawPolygonsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(triangulated == null)
+                return;
+
+            using (Graphics graphics = Graphics.FromImage(MainImage.Image))
+            {
+                foreach (Triangle t in triangulated.Triangles)
+                {
+                    PointF[] points =
+                    {
+                        new PointF((float) t.GetVertex(0).X, (float) t.GetVertex(0).Y),
+                        new PointF((float) t.GetVertex(1).X, (float) t.GetVertex(1).Y),
+                        new PointF((float) t.GetVertex(2).X, (float) t.GetVertex(2).Y),
+                    };
+
+                    Bresenham4Line(graphics, Color.Red, (int)points[0].X, (int)points[0].Y, (int)points[1].X, (int)points[1].Y);
+                    Bresenham4Line(graphics, Color.Red, (int)points[1].X, (int)points[1].Y, (int)points[2].X, (int)points[2].Y);
+                    Bresenham4Line(graphics, Color.Red, (int)points[2].X, (int)points[2].Y, (int)points[0].X, (int)points[0].Y);
+                }
+            }
+            MainImage.Invalidate();
         }
     }
 }
